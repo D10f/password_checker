3dfaef54f954e577e5fbda098755b54a9440f130
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2025-07-25 14:31:12 -0400

    Implement RFC-compliant URL validation
    This commit replaces the validation method from the unsafe urllib-based
     to a custom RFC-compliant regex pattern.
    Also added a new content property to access the content string of the
    request, to be sent through the socket, instead of providing the socket
    itself as a parameter.

--- Request.py
+++ Request.py
@@ -1,39 +1,47 @@
+import re
 from Headers import Headers
-from urllib.parse import urlparse, urljoin
-import socket
 
+UNRESERVED = r"[a-zA-Z0-9-._~]"
+PCT_ENC    = r"%[a-fA-F0-9]{2}"
+SUB_DELIMS = r"[!$&'()*+,;=]"
+PCHAR      = rf"({UNRESERVED}|{PCT_ENC}|{SUB_DELIMS}|:|@)"
+
+URL_MATCHER = re.compile(rf"""^
+    # Path component
+    (
+        (/({{PCHAR}}+(/{{PCHAR}}*)*)?)
+        | (/{{PCHAR}}*)*
+        | ({{PCHAR}}+(/{{PCHAR}}*)*)
+        | ({{PCHAR}}{0})
+    )
+    
+    # Query component
+    (\?({{PCHAR}}|\/|\?)*)?
+    
+    # Fragment component
+    (\#({{PCHAR}}|\/|\?)*)?
+""".replace("{{PCHAR}}", PCHAR), re.VERBOSE)
+
+def validate_path_or_throw(path: str):
+    if not URL_MATCHER.match(path):
+        raise ValueError(f"Invalid path format: {path}")
+    return path
 
 class Request:
     def __init__(self, method: str, path: str, headers: dict | Headers = None, body: str = None, **kwargs):
         self.method = method.upper()
-        self.path = Request.validate_path(path)
         self.headers = Headers(headers if Headers else {})
+        self.path = validate_path_or_throw(path)
         self.body = body if method in ["POST", "PATCH", "PUT"] else None
         self.timeout = kwargs.get("timeout")
         
 
-    @staticmethod
-    def validate_path(path: str) -> str:
-        if not path.startswith('/'):
-            path = '/' + path
-
-        # Validate path can form valid URL
-        test_url = urljoin('https://example.com', path)
-        if not urlparse(test_url).path == path:
-            raise ValueError(f"Invalid path format: {path}")
-        
-        return path
-
-
-    def send(self, s: socket.socket) -> None:
+    @property
+    def content(self):
         content = f"{self.method} {self.path} HTTP/1.1\r\n"
         content += str(self.headers) + "\r\n\r\n"
 
         if self.body:
             content += self.body
 
-        if self.timeout:
-            current_timeout = s.gettimeout()
-            s.settimeout(self.timeout)
-
-        s.sendall(content.encode())
+        return content.encode()
